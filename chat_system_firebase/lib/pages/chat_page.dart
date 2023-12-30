2240c0f7ac62eb8240a12c387dfbd31a73afec0d
KRSL367 <krishalpudasaini12345@gmail.com>
2023-12-31 00:01:53 +0545

    Chat between two users is created sucessfully

--- chat_system_firebase/lib/pages/chat_page.dart
+++ chat_system_firebase/lib/pages/chat_page.dart
@@ -1,3 +1,8 @@
+import 'package:chat_system_firebase/components/chat_bubble.dart';
+import 'package:chat_system_firebase/components/my_text_field.dart';
+import 'package:chat_system_firebase/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -14,11 +19,126 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messagecontroller = TextEditingController();
+  final ChatService _chatService = ChatService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void sendMessage() async {
+    //only send message if there is somthing to send
+    if (_messagecontroller.text.isNotEmpty) {
+      await _chatService.sendMessage(
+          widget.receiverUserID, _messagecontroller.text);
+      //clear the controller after sending the message
+      _messagecontroller.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.receiverUserEmail),
+      ),
+      body: Column(
+        children: [
+          //message
+          Expanded(
+            child: _buildMessageList(),
+          ),
+          //user input
+          _buildMessageInput(),
+
+          const SizedBox(
+            height: 25,
+          ),
+        ],
+      ),
+    );
+  }
+  //build message list
+
+  Widget _buildMessageList() {
+    return StreamBuilder(
+      stream: _chatService.getMessages(
+          widget.receiverUserID, _firebaseAuth.currentUser!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error${snapshot.error}');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('Loading...');
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map(
+                (document) => _buildMessageItem(document),
+              )
+              .toList(),
+        );
+      },
+    );
+  }
+
+  //build message item
+  Widget _buildMessageItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+
+    //align the message to the right if the sender is current user, otherwise to the left
+    var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
+        ? Alignment.centerRight
+        : Alignment.centerLeft;
+
+    return Container(
+      alignment: alignment,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment:
+              (data['senderId'] == _firebaseAuth.currentUser!.uid)
+                  ? CrossAxisAlignment.end
+                  : CrossAxisAlignment.start,
+          mainAxisAlignment:
+              (data['senderId'] == _firebaseAuth.currentUser!.uid)
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
+          children: [
+            Text(data['senderEmail']),
+            const SizedBox(
+              height: 8,
+            ),
+            ChatBubble(message: data['message']),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //build message input
+
+  Widget _buildMessageInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Row(
+        children: [
+          //textfield
+          Expanded(
+            child: MyTextField(
+              controller: _messagecontroller,
+              hinttext: 'Enter message',
+              obscuretext: false,
+            ),
+          ),
+
+          //send button
+
+          IconButton(
+            onPressed: sendMessage,
+            icon: const Icon(
+              Icons.arrow_upward,
+              size: 40,
+            ),
+          ),
+        ],
       ),
     );
   }
